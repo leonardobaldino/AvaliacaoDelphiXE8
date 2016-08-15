7add2fde09f3dc8d3ee6e73fabb9de395ff74223
leonardobaldino <leonardobaldino@gmail.com>
2016-08-14 22:28:30 -0300

    Add files via upload

---
+++ Avaliacao_CadastroProduto/ADMINISTRADOR/ADMINISTRADOR.dpr
@@ -0,0 +1,19 @@
+program ADMINISTRADOR;
+
+uses
+  madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+  System.StartUpCopy,
+  FMX.Forms,
+  unMain in 'unMain.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
